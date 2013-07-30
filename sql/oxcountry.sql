b2668c97ff1a6e68c7e83f8cb731b027ea709995
Dominik Smatana <dominik.smatana@gmail.com>
2013-07-30 16:39:27 +0200

    translation of the most important country names

---
+++ sql/oxcountry.sql
@@ -0,0 +1,14 @@
+
+UPDATE `oxcountry` SET `OXTITLE_2` = `OXTITLE`, `OXSHORTDESC_2` = `OXSHORTDESC`, `OXLONGDESC_2` = `OXLONGDESC`;
+
+/* the most important countries */
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Iný štát' WHERE `OXID` = '2db455824e4a19cc7.14731328';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Nemecko' WHERE `OXID` = 'a7c40f631fc920687.20179984';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Rakúsko' WHERE `OXID` = 'a7c40f6320aeb2ec2.72885259';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Česká republika' WHERE `OXID` = '8f241f110957cb457.97820918';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Maďarsko' WHERE `OXID` = '8f241f11095b3e016.98213173';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Poľsko' WHERE `OXID` = '8f241f1109624d3f8.50953605';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Slovensko' WHERE `OXID` = '8f241f1109647a265.29938154';
+UPDATE `oxcountry` SET `OXTITLE_2` = 'Ukrajina' WHERE `OXID` = '8f241f110968391d2.37199812';
+
+
