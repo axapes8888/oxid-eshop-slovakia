4835b0c4df37a668c35474ff561340d63d699b8b
Dominik Smatana <dominik.smatana@gmail.com>
2013-07-30 15:36:16 +0200

    translation of the names of the US states

---
+++ sql/oxstates.sql
@@ -0,0 +1,15 @@
+
+UPDATE `oxstates` SET `OXTITLE_2` = `OXTITLE`;
+
+UPDATE `oxstates` SET `OXTITLE_2` = 'Aljaška' WHERE `OXID` = 'AK';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Havaj' WHERE `OXID` = 'HI';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Južná Dakota' WHERE `OXID` = 'SD';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Južná Karolína' WHERE `OXID` = 'SC';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Kalifornia' WHERE `OXID` = 'CA';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Nové Mexiko' WHERE `OXID` = 'NM';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Pensylvánia' WHERE `OXID` = 'PA';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Severná Karolína' WHERE `OXID` = 'NC';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Severná Dakota' WHERE `OXID` = 'ND';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Virgínia' WHERE `OXID` = 'VA';
+UPDATE `oxstates` SET `OXTITLE_2` = 'Západná Virgínia' WHERE `OXID` = 'WV';
+
