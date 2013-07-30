4a7b3c523ae0b41bcfe8182916775e7d2b5793af
Dominik Smatana <dominik.smatana@gmail.com>
2013-07-30 15:08:39 +0200

    translation of groups names

---
+++ sql/oxgroups.sql
@@ -0,0 +1,18 @@
+
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Blacklist' WHERE `OXID` = 'oxidblacklist';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Malý obrat' WHERE `OXID` = 'oxidsmallcust';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Stredne veľký obrat' WHERE `OXID` = 'oxidmiddlecust';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Veľký obrat' WHERE `OXID` = 'oxidgoodcust';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Zahraniční zákazníci' WHERE `OXID` = 'oxidforeigncustomer';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Domáci zákazníci' WHERE `OXID` = 'oxidnewcustomer';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Powershopper' WHERE `OXID` = 'oxidpowershopper';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Maloobchodníci' WHERE `OXID` = 'oxiddealer';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Odberatelia noviniek' WHERE `OXID` = 'oxidnewsletter';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Administrátori obchodu' WHERE `OXID` = 'oxidadmin';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Cena A' WHERE `OXID` = 'oxidpricea';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Cena B' WHERE `OXID` = 'oxidpriceb';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Cena C' WHERE `OXID` = 'oxidpricec';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Zablokovaní' WHERE `OXID` = 'oxidblocked';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Zákazníci' WHERE `OXID` = 'oxidcustomer';
+UPDATE `oxgroups` SET `OXTITLE_2` = 'Zatiaľ nič nekúpili' WHERE `OXID` = 'oxidnotyetordered';
+
